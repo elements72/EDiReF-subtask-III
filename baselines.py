9a9fd95966699d2164257ea2ee623134a0900076
alessandrablasioli <itsalsblashere@gmail.com>
2024-01-22 16:23:41 +0100

    modified random and majority classiifier

--- baselines.py
+++ baselines.py
@@ -32,28 +32,76 @@ class BertBaseline(ClassificationTaskModel):
         trigger_logits = self.trigger_clf(encoded_utterances)
         return emotion_logits.to(device), trigger_logits.to(device)
 
+'''
+Random Classifier
 
+'''
 class RandomUniformClassifier(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, num_emotion_classes, num_trigger_classes):
         super().__init__()
         self._random_state = np.random.RandomState()
+        self.num_emotion_classes = num_emotion_classes
+        self.num_trigger_classes = num_trigger_classes
 
-    def predict(self, X):
+    def determine_num_classes(self, dataloader, key):
+        sample_batch = next(iter(dataloader))
+        num_classes = sample_batch[key].size(1)
+        return num_classes
+
+    def initialize_classes(self, train_dataloader):
+        self.num_emotion_classes = self.determine_num_classes(train_dataloader, "emotions")
+        self.num_trigger_classes = self.determine_num_classes(train_dataloader, "triggers")
+
+    def predict_emotions(self, X):
         batch_size = X.size(0)
-        logits = self._random_state.uniform(size=(batch_size, 6))
-        logits = logits > 0.5
-        return torch.tensor(logits, dtype=torch.float32).to(device)
+        emotions_logits = self._random_state.uniform(size=(batch_size, self.num_emotion_classes))
+        emotions_predictions = torch.tensor(emotions_logits > 0.5, dtype=torch.float32).to(device)
+        return emotions_predictions
 
+    def predict_triggers(self, X):
+        batch_size = X.size(0)
+        triggers_logits = self._random_state.uniform(size=(batch_size, self.num_trigger_classes))
+        triggers_predictions = torch.tensor(triggers_logits > 0.5, dtype=torch.float32).to(device)
+        return triggers_predictions
 
+'''
+Majority Classifier
+
+'''
 class MajorityClassifier(pl.LightningModule):
     def __init__(self):
         super().__init__()
+        self.majority_emotions_class = None
+        self.majority_triggers_class = None
 
     def fit(self, train_dataloader):
-        labels = torch.cat([batch["labels"] for batch in train_dataloader])
-        majority_class = labels.mode()[0].item()
-        self.majority_class = torch.tensor([1 if i == majority_class else 0 for i in range(self.num_classes)])
+       
+        emotions_list = []
+        triggers_list = []
+        for batch in train_dataloader:
+            emotions = batch["emotions"]
+            triggers = batch["triggers"]
 
-    def predict(self, x):
-        batch_size = x.size(0)
-        return self.majority_class.repeat(batch_size, 1)
+        if emotions.size(1) != triggers.size(1):
+                raise ValueError("Dimension mismatch along the second dimension.")
+        
+        emotions_list.append(emotions)
+        triggers_list.append(triggers)
+        emotions_labels = torch.cat(emotions_list, dim=0)
+        triggers_labels = torch.cat(triggers_list, dim=0)
+
+    
+        unique_emotions, counts_emotions = torch.unique(emotions_labels, return_counts=True)
+        majority_emotions_class = unique_emotions[counts_emotions.argmax()].item()
+
+        unique_triggers, counts_triggers = torch.unique(triggers_labels, return_counts=True)
+        majority_triggers_class = unique_triggers[counts_triggers.argmax()].item()
+
+        self.majority_emotions_class = majority_emotions_class
+        self.majority_triggers_class = majority_triggers_class
+    
+    def predict_emotions(self, x):
+        return torch.tensor([self.majority_emotions_class] * len(x))
+
+    def predict_triggers(self, x):
+        return torch.tensor([self.majority_triggers_class] * len(x))
